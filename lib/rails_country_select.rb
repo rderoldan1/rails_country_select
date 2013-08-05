4140cea19155560fbae3870d45f8bf34ad7bf584
Ruben Espinosa <rderoldan1@gmail.com>
2013-08-04 21:58:15 -0500

    Bump to 0.1.0

--- lib/rails_country_select.rb
+++ lib/rails_country_select.rb
@@ -1,10 +1,11 @@
 # encoding: utf-8
 require "rails_country_select/version"
-
 require File.expand_path('../country_definitions', __FILE__)
+
 if defined?(Rails) && defined?(ActionView)
   module ActionView
     module Helpers
+      autoload :CountryHelper, 'rails_country_select/helpers/rails_country_select'
       module FormOptionsHelper
         def country_select(object, method, options = {}, html_options = {})
           options[:keys]    = :names unless options.has_key?(:keys)
@@ -26,6 +27,17 @@ if defined?(Rails) && defined?(ActionView)
         def country_select(method, options = {}, html_options = {})
           @template.country_select(@object_name, method, options.merge({:object => @object}), html_options)
         end
+      end
+
+      # get country info given some params
+      def country(options = {:name => "", :num =>"", :alpha2s => "", :alpha3s => ""})
+        result = []
+        COUNTRY_NUMS.zip(COUNTRY_NAMES, COUNTRY_ALPHA2S, COUNTRY_ALPHA3S).each do |country|
+          if options[:nums].to_i.eql?(country[0]) || options[:name].eql?(country[1]) || options[:alpha2s].eql?(country[2]) || options[:alpha3s].eql?(country[3])
+            result = country
+          end
+        end
+        result
       end
     end
   end
