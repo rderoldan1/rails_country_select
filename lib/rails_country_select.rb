73290350643728179b02c90de291167e3aeed281
Ruben Espinosa <rderoldan1@gmail.com>
2014-03-28 13:43:42 -0500

    prueba 2

--- lib/rails_country_select.rb
+++ lib/rails_country_select.rb
@@ -19,7 +19,8 @@ require File.expand_path('../country_definitions', __FILE__)
           }
           #options = options.delete(:object)
           select_options = potential[options[:keys]].zip(potential[options[:values]])
-          Tags::Select.new(object, method, self, select_options, options, html_options)
+          select_tag(method, options_for_select(select_options),options,html_options)
+          #Tags::Select.new(object, method, self, select_options, options, html_options)
         end
       end
 
